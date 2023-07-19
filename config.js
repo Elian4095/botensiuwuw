f03b908d1c0c62041ee0bc1ab423733709be227a
Elian4095 <88990955+Elian4095@users.noreply.github.com>
2023-07-19 18:36:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
+  ['5493517719919', 'Elián', true],
   ['5492622271736'], 
   ['5492622271736'] 
 ] //Numeros de owner 
